4d1164d61a1a8048a82acff81866570a3672b268
JuliaRyzhova <16-j.lazy@bk.ru>
2022-12-01 13:43:39 +0300

    Added decision Task

---
+++ Final_work/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+string[] CreatetArray3CharsMAX(string[] arr)
+{
+    int lengthMax = 3;
+    int count = 0;
+    string[] resultArray = new string[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= lengthMax)
+        {
+            resultArray[count] = arr[i];
+            count++;
+        }
+    }
+    return resultArray;
+
+}
+
+void ShowArray(string[] array, string info)
+{
+    Console.WriteLine(info);
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+string[] array = new string[] { "hello", "2", "world", ":)" };
+ShowArray(array, "The source array is: ");
+
+string[] resultArray = CreatetArray3CharsMAX(array);
+ShowArray(resultArray, "The new created array is: ");
